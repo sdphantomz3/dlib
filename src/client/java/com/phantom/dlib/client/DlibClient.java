9dc97541da48bebaaa9ab91d2637079e0f38ff79
PhantomZ3 <sd.phantomz3@gmail.com>
2026-06-20 14:15:27 +0500

    Initial commit

---
+++ src/client/java/com/phantom/dlib/client/DlibClient.java
@@ -0,0 +1,10 @@
+package com.phantom.dlib.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DlibClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
