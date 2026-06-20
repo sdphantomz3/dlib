759d30aa083a6f798ea3ca18980215a97444524a
PhantomZ3 <sd.phantomz3@gmail.com>
2026-06-20 14:20:23 +0500

    add categories

---
+++ src/client/java/com/phantom/dlib/client/api/ConfigCategory.java
@@ -0,0 +1,21 @@
+package com.phantom.dlib.client.api;
+
+import com.phantom.dlib.client.api.entry.ConfigEntry;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConfigCategory {
+    private final String title;
+    private final List<ConfigEntry> entries = new ArrayList<>();
+
+    public ConfigCategory(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() { return title; }
+    public List<ConfigEntry> getEntries() { return entries; }
+
+    public void addEntry(ConfigEntry entry) {
+        this.entries.add(entry);
+    }
+}
---
+++ src/client/java/com/phantom/dlib/client/api/ModConfig.java
@@ -0,0 +1,22 @@
+package com.phantom.dlib.client.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModConfig {
+    private final String modName;
+    private final List<ConfigCategory> categories = new ArrayList<>();
+
+    public ModConfig(String modName) {
+        this.modName = modName;
+    }
+
+    public String getModName() { return modName; }
+    public List<ConfigCategory> getCategories() { return categories; }
+
+    public ConfigCategory createCategory(String categoryTitle) {
+        ConfigCategory category = new ConfigCategory(categoryTitle);
+        this.categories.add(category);
+        return category;
+    }
+}
